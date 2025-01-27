93771b08d8867b719f5f5380fcae7b28ea7a3994
WonJin <try3982@kakao.com>
2025-01-27 14:12:16 +0900

    프로젝트 생성, dto 패키지에 CreateAccount 클래스 생성

--- src/main/java/com/example/account/controller/AccountController.java
+++ src/main/java/com/example/account/controller/AccountController.java
@@ -4,9 +4,9 @@ import com.example.account.domain.Account;
 import com.example.account.service.AccountService;
 import com.example.account.service.RedisTestService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,16 +14,20 @@ public class AccountController {
     private final AccountService accountService;
     private final RedisTestService redisTestService;
 
+    @PostMapping("/account")
+    public String createAccount(
+            @RequestBody @Valid CreateAccount
+    ) {
+        accountService.createAccount();
+        return "success";
+    }
+
     @GetMapping("/get-lock")
     public String getLock() {
         return redisTestService.getLock();
     }
 
-    @GetMapping("/create-account")
-    public String createAccount() {
-        accountService.createAccount();
-        return "success";
-    }
+
 
     @GetMapping("/account/{id}")
     public Account getAccount(
